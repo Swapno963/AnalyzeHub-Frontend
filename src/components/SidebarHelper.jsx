37eeb57dbd44b99ae0782a06923b7c8c198406c4
swapno963 <swapno963@gmail.com>
2024-08-16 23:30:10 +0600

    Made side bar functional

--- src/components/SidebarHelper.jsx
+++ src/components/SidebarHelper.jsx
@@ -40,7 +40,7 @@ export default function SidebarHelper({ link }) {
                 transitionProperty: "max-height, opacity",
               }}
             >
-              {ch?.title}
+              <a href={`/${link?.url}`}>{ch?.title}</a>
             </p>
           ))
         }
